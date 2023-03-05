55566f87b495370b28623fd6ca506ed236665772
RenzouM <renzoum@gmail.com>
2023-03-05 02:00:02 -0300

    TAMOOOOOOOOOO

--- index.js
+++ index.js
@@ -25,11 +25,11 @@ const connectDB = async () => {
   }
 };
 
-// connectDB().then(() => {
-//   app.listen(PORT, () => {
-//       console.log("listening for requests");
-//   })
-// })
+connectDB().then(() => {
+  app.listen(PORT, () => {
+      console.log("listening for requests");
+  })
+})
 
 main().catch((err) => console.log(err));
 
