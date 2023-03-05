6a508fd46ff79fe175db477ffeaed50cc5059302
RenzouM <renzoum@gmail.com>
2023-03-05 03:47:15 -0300

    aqqqqqqq

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(express.static("public"));
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(MONGO_URI, {useNewUrlParser: true});
+    const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.log(error);
