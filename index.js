0d0db680b180c150189a24efb4beda7c57484ac5
RenzouM <renzoum@gmail.com>
2023-03-05 23:01:27 -0300

    DDDW

--- index.js
+++ index.js
@@ -107,7 +107,7 @@ app.post("/", function (req, res) {
     item.save();
     res.redirect("/");
   } else {
-    List.findOne({ name: listName }).then(function (error, foundList) {
+    List.findOne({ name: listName }, function (error, foundList) {
       foundList.items.push(item);
       foundList.save();
       res.redirect("/" + listName);
